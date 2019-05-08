44a85121845351d3134af3aa749cb48347adcad6
justingarrigus <justin.garrigus.341@k12.friscoisd.org>
2019-05-07 23:31:25 -0500

    Started Inventory Screen

---
+++ JModelling/JModelling/Inventory/Item.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JModelling.Inventory
+{
+    /// <summary>
+    /// A basic interface defining what an item should be. When
+    /// creating an inventory item, have it implement this class. 
+    /// </summary>
+    interface Item
+    {
+        /// <summary>
+        /// How this item should look inside your inventory menu. 
+        /// </summary>
+        Texture2D ImageInInventory
+        {
+            get;
+            set;
+        } 
+    }
+}
